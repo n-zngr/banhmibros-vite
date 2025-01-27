d6f84b8cd164a269d1119fa942d50004ed529c3b
deltagamingch <deltagaming.ch.nik@gmail.com>
2025-01-27 12:00:24 +0100

    build(routing): implement routing

--- app/main.tsx
+++ app/main.tsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import './index.css';
 import '../fonts/circula.css';
 import '../fonts/poppins.css';
-import App from './App.tsx'
+import App from './App.tsx';
+import Menu from './components/Menu.tsx';
 
 const passwordPrompt = (): void => {
     const validPassword = "test";
@@ -26,7 +28,13 @@ const passwordPrompt = (): void => {
 passwordPrompt();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
