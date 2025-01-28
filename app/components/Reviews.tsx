ff3875467bc0dc921b6eb98bc998281aa2c1e964
deltagamingch <deltagaming.ch.nik@gmail.com>
2025-01-28 22:40:32 +0100

    style(reviews): minor styling changes for responsiveness

--- app/components/Reviews.tsx
+++ app/components/Reviews.tsx
@@ -2,7 +2,7 @@ import ReviewItem from "./Reviews/ReviewItem";
 
 export default function Reviews() {
     return (
-        <div className="bg-black-500 grid grid-cols-2 px-32 2xl:px-64 py-64 2xl:py-96 gap-12">
+        <div className="bg-black-500 grid grid-cols-2 px-8 md:px-16 lg:px-32 2xl:px-64 py-16 md:py-32 lg:py-64 2xl:py-96 gap-12">
             <ReviewItem>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, eius!</ReviewItem>
             <ReviewItem>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, eius!</ReviewItem>
             <ReviewItem>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias, eius!</ReviewItem>
