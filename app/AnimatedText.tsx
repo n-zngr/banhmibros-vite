0bf7a1483db24d7b78c5f5c87758bf3bffd8513c
deltagamingch <deltagaming.ch.nik@gmail.com>
2025-01-29 21:36:09 +0100

    fix(animated-text): update animation to ensure component hydration on page load

--- app/AnimatedText.tsx
+++ app/AnimatedText.tsx
@@ -11,26 +11,30 @@ const AnimatedText: React.FC<AnimatedTextProps> = ({ text, scrollRef }) => {
     const { scrollYProgress } = useScroll({
         target: ref,
         container: scrollRef,
-        offset: ["0 1", "0 0.25"]
+        offset: ["0 1", "0 0.25"],
+        layoutEffect: false
     });
+
+    const defaultColor = "#8E8983";
+    const targetColor = "#1D1A17";
 
     return (
         <div ref={ref} style={{ overflow: "hidden" }}>
-            {text.split("").map((char, index) => {
-                const colorProgress = useTransform(
-                    scrollYProgress,
-                    [index / text.length, (index + 1) / text.length],
-                    ["#8E8983", "#1D1A17"]
-                );
-                return (
+            {text.split("").map((char, index) => (
                 <motion.span
                     key={index}
-                    style={{ display: "inline-block", color: colorProgress }}
+                    style={{
+                        display: "inline-block",
+                        color: useTransform(
+                            scrollYProgress,
+                            [index / text.length, (index + 1) / text.length],
+                            [defaultColor, targetColor]
+                        )
+                    }}
                 >
                     {char}
                 </motion.span>
-                );
-            })}
+            ))}
         </div>
     );
 };
